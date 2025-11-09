afa61c0ee52562f9877fa0484f06e03f1d7bcfb3
Orlando Altamirano Vargas <orlandoaltamirano85@gmail.com>
2025-11-08 22:15:53 -0400

    Avance clase 07/10/2025

---
+++ src/config/database.config.ts
@@ -0,0 +1,20 @@
+// src\config\database.config.ts
+import { Sequelize } from "sequelize";
+
+import { ENV } from "./env.config";
+
+export const sequelize = new Sequelize(
+  ENV.POSTGRES_DB,
+  ENV.POSTGRES_USER,
+  ENV.POSTGRES_PASSWORD,
+  {
+    host: ENV.PGHOST,
+    port: Number(ENV.PGPORT),
+    logging: true,
+    dialect: 'postgres',
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  }
+)
